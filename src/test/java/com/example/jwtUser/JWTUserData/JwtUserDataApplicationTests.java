a5ae9b1035fa899fd0865cb5be4df1442eb4a536
Saurabh Kumar Tiwari <sky.royaltiwari@gmail.com>
2022-11-22 09:03:08 +0530

    Initial commit

---
+++ src/test/java/com/example/jwtUser/JWTUserData/JwtUserDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwtUser.JWTUserData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtUserDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
